2a2862ae7b2e9cffb84369dcd1505e9a2fab6f6b
Zachary Abbott <zax.zm2@gmail.com>
2016-12-19 20:28:06 -0700

    got the random map funtion to dysplay.
    changed the map to a multi array, and added a function to convert the
    map to html.

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -1,18 +1,36 @@
+Array.prototype.random = function () {
+  return this[Math.floor((Math.random()*this.length))];
+}
+
+tyletypes = ['grass', 'water', 'genaric']; 
 map = [];
 
-function tile(type, pos){
+function tile(type){
     this.type = type;
-    this.pos = pos;
 };
  
 function genGinaricMap (xSize, ySize) {
-     for(var i = 0; i <= xSize; i++){
-         for(var j = 0; j <= ySize; j++){
-             map.push(new tile('genaric',{"x":i,"y":j}));
+     for(var i = 0; i <= xSize-1; i++){
+         map.push([]);
+         for(var j = 0; j <= ySize-1; j++){
+             map[i].push(new tile(tyletypes.random()));
          }
          
      }
  }
+function startMapHTML (map){
+    elem = "";
+    for(var i = 0; i <= map.length-1; i++){
+        elem += "<div class='row' id='"+"row_"+i+"'>";
+        for(var j = 0; j <= map[i].length-1; j++){
+            elem += "<div class='tile "+ map[i][j].type +"' id='"+"col_"+(j)+"'></div>";
+        };
+        elem +="</div>"
+    };
+    return elem;
+};
+
 
 genGinaricMap(10,10);
-console.log(map, map[0], map[0].pos);
+console.log(map, map[0][0], map[0][0].type);
+document.getElementById('map').innerHTML += startMapHTML(map);
